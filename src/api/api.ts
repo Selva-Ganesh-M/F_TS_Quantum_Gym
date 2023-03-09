ced28a5da9c5e8433287466af7da876690e3ba00
Selva Ganesh M <selvadev2k@gmail.com>
2023-03-09 18:08:12 +0530

    updated hosted server url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 export type TPayload<T> = {
-  statusText: string,
-  statusCode: number,
-  message: string,
-  payload: T
-}
+  statusText: string;
+  statusCode: number;
+  message: string;
+  payload: T;
+};
 
 export const api = axios.create({
-    baseURL: 'http://localhost:4000/api',
-    // timeout: 1000*3,
-    withCredentials: true,
-    headers: {'X-Custom-Header': 'foobar'},
-  });
+  baseURL: "https://quantum-gym-server.onrender.com/api",
+  // timeout: 1000*3,
+  withCredentials: true,
+  headers: { "X-Custom-Header": "foobar" },
+});
